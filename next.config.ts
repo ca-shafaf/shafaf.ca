9f27613048023fe8deadc7c5963bdf349400b2bf
ca-shafaf <ca-shafaf@users.noreply.github.com>
2025-06-17 12:52:59 +0000

    Simplify deployment configuration and use GitHub Pages action

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true,
-    domains: ['images.pexels.com'],
   },
   reactStrictMode: true,
-  trailingSlash: true,
 }
 
 export default nextConfig
